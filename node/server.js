4750d803a2b16534a860280eeb5f49e040be2ce5
onikah <mona.kwamboka@gmail.com>
2016-08-05 23:03:11 +0300

    HTTP server test

--- node/server.js
+++ node/server.js
@@ -1,1 +1,7 @@
-var http = require("http");
+const http = require('http');
+
+http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-type': 'plain/text' });
+  response.write('Hello World');
+  response.end();
+}).listen(8888);
