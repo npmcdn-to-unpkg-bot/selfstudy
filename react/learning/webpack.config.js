872df5069d61c6da45a31c37dba2eaa872c08d4b
onikah <mona.kwamboka@gmail.com>
2016-08-08 11:02:21 +0300

    Add preloaders

--- react/learning/webpack.config.js
+++ react/learning/webpack.config.js
@@ -5,12 +5,20 @@ module.exports = {
   },
   watch: true,
   modules: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'jshint-loader'
+      }
+    ],
     loaders: [
       {
         test: /\.es6$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
+          cacheDirectory: true,
           presets: ['react', 'es2015']
         }
       }
